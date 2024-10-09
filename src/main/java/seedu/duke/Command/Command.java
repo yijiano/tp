38b17f4a00886a922feb5edfc1252003eb66fe2e
yakultbottle <e1121147@u.nus.edu>
2024-10-09 16:42:03 +0800

    Create abstract class Command

---
+++ src/main/java/seedu/duke/Command/Command.java
@@ -0,0 +1,28 @@
+package seedu.duke.Command;
+
+import java.util.Scanner;
+
+/**
+ * Represents a command that can be executed.
+ * Each command is responsible for performing a specific action in the system.
+ */
+public abstract class Command {
+    /**
+     * Executes the command with the specified task list, user interface, and storage.
+     *
+     * @param tasks       The task list to be manipulated by the command.
+     * @param ui          The Scanner that takes input from the terminal.
+     * @param storagePath The storage path for saving/loading tasks.
+     */
+    public abstract void execute(String[] tasks, Scanner ui, String storagePath);
+
+    /**
+     * Determines whether this command will exit the application.
+     * Overridden only by the ByeCommand.
+     *
+     * @return false, as most commands do not exit the application.
+     */
+    public boolean isExit() {
+        return false;
+    }
+}
