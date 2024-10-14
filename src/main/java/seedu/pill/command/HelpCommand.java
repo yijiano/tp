50d85c70c800cd23492ae946bc3aaf9fdf55293d
Philip Chang <philip.chang130499@protonmail.com>
2024-10-14 18:33:03 +0800

    Help Command // improved 'help' and error messages

--- src/main/java/seedu/pill/command/HelpCommand.java
+++ src/main/java/seedu/pill/command/HelpCommand.java
@@ -9,20 +9,28 @@ import seedu.pill.util.Storage;
  */
 public class HelpCommand extends Command {
     private String commandName;
+    private boolean verbose;
 
     public HelpCommand() {
         this.commandName = null;
+        this.verbose = false;
     }
 
     public HelpCommand(String commandName) {
         this.commandName = commandName;
+        this.verbose = false;
+    }
+
+    public HelpCommand(String commandName, boolean verbose) {
+        this.commandName = commandName;
+        this.verbose = verbose;
     }
 
     /**
      * Executes the help command by displaying information about available commands.
-     * @param itemList The item list to be manipulated by the command.
-     * @param storage
-     * @throws PillException
+     * @param itemList       - The item list to be manipulated by the command.
+     * @param storage        -
+     * @throws PillException -
      */
     @Override
     public void execute(ItemList itemList, Storage storage) throws PillException {
@@ -44,41 +52,103 @@ public class HelpCommand extends Command {
         System.out.println("  list    - Lists all items");
         System.out.println("  quit    - Exits the program");
         System.out.println("Type 'help <command>' for more information on a specific command.");
+        System.out.println("Add  '-v' after the command for verbose output with examples.");
     }
 
     /**
-     * Shows detailed help information for each command.
-     * @param command - optional user input that determines which information is displayed.
+     * Calls the appropriate method depending on what the user requests help for.
+     * @param command - optional user input that determines which help information is displayed to the user.
      */
     private void showSpecificHelp(String command) {
         switch (command.toLowerCase()) {
         case "help":
-            System.out.println("help: Shows help information about available commands.");
-            System.out.println("Usage: help [command]");
-            System.out.println("  [command] - Optional. Specify a command to get detailed help.");
+            showHelpHelp();
             break;
         case "add":
-            System.out.println("add: Adds a new item to the inventory.");
-            System.out.println("Usage: add <name> <quantity> <price>");
-            System.out.println("  <name>     - Name of the item");
-            System.out.println("  <quantity> - Initial quantity of the item");
+            showAddHelp();
             break;
         case "delete":
-            System.out.println("delete: Removes an item from the inventory.");
-            System.out.println("Usage: delete <index>");
-            System.out.println("  <name> - Name of the item to delete (as shown in the list)");
+            showDeleteHelp();
             break;
         case "list":
-            System.out.println("list: Displays all items in the inventory.");
-            System.out.println("Usage: list");
+            showListHelp();
             break;
         case "quit":
-            System.out.println("quit: Exits the program.");
-            System.out.println("Usage: quit");
+            showQuitHelp();
             break;
         default:
             System.out.println("Unknown command: " + command);
-            System.out.println("Type 'help' for a list of available commands.");
+            System.out.println("Available commands: help, add, delete, list, quit");
+            System.out.println("Type 'help <command>' for more information on a specific command.");
+        }
+    }
+
+    /**
+     * Prints detailed information about the 'help' command.
+     */
+    private void showHelpHelp() {
+        System.out.println("help: Shows help information about available commands.");
+        System.out.println("Usage: help [command] [-v]");
+        System.out.println("  [command] - Optional. Specify a command to get detailed help.");
+        System.out.println("  [-v]      - Optional. Show verbose output with examples.");
+        if (verbose) {
+            System.out.println("\nExamples:");
+            System.out.println("  help");
+            System.out.println("  help add");
+            System.out.println("  help add -v");
+        }
+    }
+
+    /**
+     * Prints detailed information about the 'add' command.
+     */
+    private void showAddHelp() {
+        System.out.println("add: Adds a new item to the inventory.");
+        System.out.println("Usage: add <name> <quantity> <price>");
+        System.out.println("  <name>     - Name of the item");
+        System.out.println("  <quantity> - Initial quantity of the item");
+        if (verbose) {
+            System.out.println("\nExample:");
+            System.out.println("  add Aspirin 100");
+        }
+        System.out.println("\nCorrect input format: add <name> <quantity>");
+    }
+
+    /**
+     * Prints detailed information about the 'delete' command.
+     */
+    private void showDeleteHelp() {
+        System.out.println("delete: Removes an item from the inventory.");
+        System.out.println("Usage: delete <name>");
+        System.out.println("  <name> - Name of the item to delete (as shown in the list)");
+        if (verbose) {
+            System.out.println("\nExample:");
+            System.out.println("  delete Aspirin");
+        }
+        System.out.println("\nCorrect input format: delete <index>");
+    }
+
+    /**
+     * Prints detailed information about the 'list' command.
+     */
+    private void showListHelp() {
+        System.out.println("list: Displays all items in the inventory.");
+        System.out.println("Usage: list");
+        if (verbose) {
+            System.out.println("\nExample:");
+            System.out.println("  list");
+        }
+    }
+
+    /**
+     * Prints detailed information about the 'quit' command.
+     */
+    private void showQuitHelp() {
+        System.out.println("quit: Exits the program.");
+        System.out.println("Usage: quit");
+        if (verbose) {
+            System.out.println("\nExample:");
+            System.out.println("  quit");
         }
     }
 
