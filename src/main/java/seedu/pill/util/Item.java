570fbad7b35939b84bd69c01086e82a804ae6f16
yakultbottle <e1121147@u.nus.edu>
2024-10-18 23:35:45 +0800

    Update the toString, equals, and compareTo method in Item

--- src/main/java/seedu/pill/util/Item.java
+++ src/main/java/seedu/pill/util/Item.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 /**
  * Represents an item in the inventory.
  */
-public class Item {
+public class Item implements Comparable<Item> {
     private String name;
     private int quantity;
     private LocalDate expiryDate;
@@ -36,9 +36,23 @@ public class Item {
         this.quantity = quantity;
     }
 
+    /**
+     * Compares this {@code Item} with another {@code Item} based on their expiry dates.
+     * The comparison is in ascending order, meaning items with sooner expiry dates will come first.
+     *
+     * @param other The {@code Item} to be compared to this {@code Item}.
+     * @return A negative integer if this item expires sooner than the other;
+     *         a positive integer if this item expires later than the other;
+     *         zero if both items have the same expiry date.
+     */
+    @Override
+    public int compareTo(Item other) {
+        return expiryDate.compareTo(other.expiryDate);
+    }
+
     @Override
     public String toString() {
-        return name + ": " + quantity + " in stock";
+        return name + ": " + quantity + " in stock, expiring: " + expiryDate;
     }
 
     @Override
@@ -46,9 +60,9 @@ public class Item {
         if (this == obj) {
             return true;
         }
-        if (obj instanceof Item) {
-            Item item = (Item) obj;
-            return name.equals(item.getName()) && quantity == item.getQuantity();
+        if (obj instanceof Item item) {
+            return name.equals(item.getName()) && quantity == item.getQuantity()
+                    && expiryDate.equals(item.getExpiryDate());
         }
         return false;
     }
