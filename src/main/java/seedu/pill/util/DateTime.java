b8b15e18c904bf169d128f58854b83b0dcc569fd
Philip Chang <philip.chang130499@protonmail.com>
2024-10-20 14:21:07 +0800

    DateTime // Updated
    Notes:
    - Added implements Comparable for more flexibility and easier sorting.

--- src/main/java/seedu/pill/util/DateTime.java
+++ src/main/java/seedu/pill/util/DateTime.java
@@ -2,8 +2,9 @@ package seedu.pill.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-public class DateTime {
+public class DateTime implements Comparable<DateTime> {
     private LocalDateTime dateTime;
 
     public DateTime() {
@@ -35,20 +36,25 @@ public class DateTime {
         return getFormattedDateTime("HH:mm:ss");
     }
 
+    @Override
+    public int compareTo(DateTime other) {
+        return this.dateTime.compareTo(other.getDateTime());
+    }
+
     public boolean isAfter(DateTime other) {
-        return this.dateTime.isAfter(other.getDateTime());
+        return this.compareTo(other) > 0;
     }
 
     public boolean isBefore(DateTime other) {
-        return this.dateTime.isBefore(other.getDateTime());
+        return this.compareTo(other) < 0;
     }
 
     public long getDaysUntil(DateTime other) {
-        return java.time.Duration.between(this.dateTime, other.getDateTime()).toDays();
+        return ChronoUnit.DAYS.between(this.dateTime, other.getDateTime());
     }
 
     public boolean isExpired(DateTime expirationDate) {
-        return this.dateTime.isAfter(expirationDate.getDateTime());
+        return this.isAfter(expirationDate);
     }
 
     public long getDaysUntilExpiration(DateTime expirationDate) {
