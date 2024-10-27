e760a41782ef71c13b64c3dcb25eea3554e2246c
unknown <nivedit@comp.nus.edu.sg>
2024-10-27 12:15:00 +0800

    Create ExpiringCommand class

---
+++ src/main/java/seedu/pill/command/ExpiringCommand.java
@@ -0,0 +1,19 @@
+package seedu.pill.command;
+
+import seedu.pill.util.ItemMap;
+import seedu.pill.util.Storage;
+
+import java.time.LocalDate;
+
+public class ExpiringCommand extends Command {
+    private LocalDate cutOffDate;
+    public ExpiringCommand(LocalDate cutOffDate) { this.cutOffDate = cutOffDate; }
+
+    @Override
+    public void execute(ItemMap itemMap, Storage storage) {
+        itemMap.listExpiringItems(cutOffDate);
+    }
+
+    @Override
+    public boolean isExit() { return false; }
+}
