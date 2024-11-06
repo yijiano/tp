24c40e2cf274a67982d5fd16cf5169060b07a72f
unknown <nivedit@comp.nus.edu.sg>
2024-11-07 03:43:00 +0800

    Remove unused imports

--- src/test/java/seedu/pill/util/OrderTest.java
+++ src/test/java/seedu/pill/util/OrderTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 class OrderTest {
     private Order purchaseOrder;
