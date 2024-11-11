adfa89b4974b6dd9f76657396c623f220957a4cb
yijiano <zhangyijian2002@gmail.com>
2024-11-12 00:55:15 +0800

    Appreciate these CI errors

--- src/test/java/seedu/pill/util/TransactionManagerTest.java
+++ src/test/java/seedu/pill/util/TransactionManagerTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.TreeSet;
 
 class TransactionManagerTest {
     private TransactionManager transactionManager;
